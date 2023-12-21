d57e604daeb46c7d5862c3ea4f69dc64d99b0c8f
kubraaygun <kubraaygun@outlook.com.tr>
2023-12-21 23:59:26 +0300

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <main className='about'>
+    <div className="pg-header">
+      <div className="container">
+        <h1>About</h1>
+      </div>
+    </div>
+    <div className="container content">
+      <div className="row">
+        <div className="col-lg-4">
+          <h2>Why choose us.</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum eius nobis
+             harum nesciunt. Cupiditate ut fugiat, quis dicta quibusdam soluta, commodi non dolorem recusandae ab aliquid quaerat! Debitis exercitationem quia autem consequuntur
+             dolore expedita, iste dolorum atque itaque est, nisi praesentium velit quidem. Consectetur debitis dolorum nulla nihil magni accusamus!</p>
+        </div>
+        <div className="col-lg-4">
+          <h2>our mission.</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum eius nobis
+             harum nesciunt. Cupiditate ut fugiat, quis dicta quibusdam soluta, commodi non dolorem recusandae ab aliquid quaerat! Debitis exercitationem quia autem consequuntur
+             dolore expedita, iste dolorum atque itaque est, nisi praesentium velit quidem. Consectetur debitis dolorum nulla nihil magni accusamus!</p>
+        </div>
+        <div className="col-lg-4">
+          <h2>What we do.</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum eius nobis
+             harum nesciunt. Cupiditate ut fugiat, quis dicta quibusdam soluta, commodi non dolorem recusandae ab aliquid quaerat! Debitis exercitationem quia autem consequuntur
+             dolore expedita, iste dolorum atque itaque est, nisi praesentium velit quidem. Consectetur debitis dolorum nulla nihil magni accusamus!</p>
+        </div>
+      </div>
+    </div>
+   </main>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <main>
+    <div className="pg-header">
+      <div className="container">
+        <h1>Welcome</h1>
+      </div>
+      <div className="container content">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione cumque repellendus ut error aperiam quam, minima et, temporibus modi eos ad obcaecati tempore voluptatibus aspernatur exercitationem porro eligendi esse sequi doloribus necessitatibus possimus dolorum incidunt perferendis? Veritatis quam modi atque iure aliquid id officia architecto voluptatibus rerum eligendi fuga, ratione beatae totam quod voluptas minima vel inventore porro laboriosam? Impedit est dicta ex cumque facere obcaecati quaerat sapiente, voluptatum nihil corrupti. Maxime sapiente nesciunt facilis nisi porro accusantium sed aut eius, minima amet deserunt nemo repellat nam odio quas inventore deleniti illum consectetur vitae magni accusamus quo mollitia! Ratione, rem!
+      </div>
+    </div>
+   </main>
+  )
+}
+
+export default Home
